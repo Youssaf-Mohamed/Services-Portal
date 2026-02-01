609c82626abefde3d2bcfacdce110a40b23c18e2
omar algamel <omeralgamel@gmail.com>
2026-02-02 00:21:39 +0200

    .

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -6,9 +6,9 @@ use Illuminate\Foundation\Configuration\Middleware;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
-        web: __DIR__.'/../routes/web.php',
-        api: __DIR__.'/../routes/api.php',
-        commands: __DIR__.'/../routes/console.php',
+        web: __DIR__ . '/../routes/web.php',
+        api: __DIR__ . '/../routes/api.php',
+        commands: __DIR__ . '/../routes/console.php',
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
@@ -19,6 +19,11 @@ return Application::configure(basePath: dirname(__DIR__))
         $middleware->alias([
             'test.mode' => \App\Http\Middleware\TestModeOnly::class,
             'role' => \Spatie\Permission\Middleware\RoleMiddleware::class,
+        ]);
+
+        // Exclude SSO endpoints from CSRF verification
+        $middleware->validateCsrfTokens(except: [
+            'api/sso/*',
         ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
@@ -44,8 +49,10 @@ return Application::configure(basePath: dirname(__DIR__))
                 }
 
                 // Authorization errors (403)
-                if ($e instanceof \Illuminate\Auth\Access\AuthorizationException || 
-                    $e instanceof \Symfony\Component\HttpKernel\Exception\AccessDeniedHttpException) {
+                if (
+                    $e instanceof \Illuminate\Auth\Access\AuthorizationException ||
+                    $e instanceof \Symfony\Component\HttpKernel\Exception\AccessDeniedHttpException
+                ) {
                     return \App\Support\ApiResponse::error(
                         'Unauthorized',
                         null,
